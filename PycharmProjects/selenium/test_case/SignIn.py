34593ac27bc66a23020125ad23f4a02c2ac8a1bc
zhaoyoumo <youmozhao@gmail.com>
2019-04-04 21:35:23 +0800

    update

--- PycharmProjects/selenium/test_case/SignIn.py
+++ PycharmProjects/selenium/test_case/SignIn.py
@@ -35,29 +35,23 @@ class SignIn(unittest.TestCase):
     def test_01(self):
         u'''正确账户，正确密码'''
         self.SignIn("zhaoyoumo@outlook.com", "Abcd1234**")  # 调用登录方法
-        # 获取登录后的账号名称
-        text = self.driver.find_element_by_xpath("//li/div").text
+        text = self.driver.find_element_by_xpath("//li/div").text   # 获取登录后的账号名称
         print text
-        # 断言实际结果与期望结果一致
-        self.assertEqual(text, "zha******@outlook.com")
+        self.assertEqual(text, "zha******@outlook.com") # 断言实际结果与期望结果一致
         print u"登录成功！"
 
     def test_02(self):
         u'''正确账户，错误密码'''
         self.SignIn("zhaoyoumo@outlook.com", "**dddd")  # 调用登录方法
-        # 获取错误提示
-        error_message = self.driver.find_element_by_xpath("//span/div/div/div/div/div").text
-        # 断言实际结果与期望结果一致
-        self.assertTrue(error_message)
+        error_message = self.driver.find_element_by_xpath("//span/div/div/div/div/div").text    # 获取错误提示
+        self.assertTrue(error_message)  # 断言实际结果与期望结果一致
         print (u"弹出错误提示：%s" % error_message)
 
     def test_03(self):
         u'''错误账户，错误密码'''
         self.SignIn("123@qq.com", "**dddd")  # 调用登录方法
-        # 获取错误提示
-        error_message = self.driver.find_element_by_xpath("//span/div/div/div/div/div").text
-        # 断言实际结果与期望结果一致
-        self.assertTrue(error_message)
+        error_message = self.driver.find_element_by_xpath("//span/div/div/div/div/div").text    # 获取错误提示
+        self.assertTrue(error_message)  # 断言实际结果与期望结果一致
         print (u"弹出错误提示：%s" % error_message)
 
     def tearDown(self):
