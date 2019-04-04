5c0485a8f04d180edc8f5f9411e6ff60436fa04b
zhaoyoumo <youmozhao@gmail.com>
2019-04-04 11:09:13 +0800

    update

--- PycharmProjects/selenium/test_case/SignIn.py
+++ PycharmProjects/selenium/test_case/SignIn.py
@@ -45,13 +45,21 @@ class SignIn(unittest.TestCase):
 
     def test_02(self):
         u'''正确账户，错误密码'''
-        self.SignIn("123@qq.com", "**dddd")  # 调用登录方法
+        self.SignIn("zhaoyoumo@outlook.com", "**dddd")  # 调用登录方法
         # 获取错误提示
         error_message = self.driver.find_element_by_xpath("//span/div/div/div/div/div").text
         # 断言实际结果与期望结果一致
         self.assertTrue(error_message)
         print (u"弹出错误提示：%s" % error_message)
 
+    def test_03(self):
+        u'''错误账户，错误密码'''
+        self.SignIn("123@qq.com", "**dddd")  # 调用登录方法
+        # 获取错误提示
+        error_message = self.driver.find_element_by_xpath("//span/div/div/div/div/div").text
+        # 断言实际结果与期望结果一致
+        self.assertTrue(error_message)
+        print (u"弹出错误提示：%s" % error_message)
 
     def tearDown(self):
         self.driver.quit()
@@ -61,25 +69,5 @@ if __name__ == "__main__":
     unittest.main()
 
 
-# if account == driver.find_element_by_id("account").click():
-#     driver.find_element_by_xpath("//div/div/button").click()
-#     feedback_1 = driver.find_element_by_xpath("//form/div/div").text
-#     print (u"获取到的提示：%s" % feedback_1)
-#     if feedback_1 == u"请输入邮箱/手机号":
-#         print u"登录失败"
-#     else:
-#         print u"登录成功"
-#         time.sleep(1)
-#
-# if account == driver.find_element_by_id("account").send_keys("10000000000"):
-#     print u"输入错误的账户"
-#     driver.find_element_by_xpath("//div/div/button").click()
-#     feedback_2 = driver.find_element_by_id("password").text
-#     print (u"获取到的提示：%s" % feedback_2)
-#     if feedback_2 == u"输入您的登录密码":
-#         print u"密码输入为空，登录失败"
-#     else:
-#         print u"登录成功"
-#         time.sleep(1)
 
 
