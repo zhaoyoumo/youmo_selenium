2a8408ab1dab47171d4d3bdc580842b2abfb0dd1
zhaoyoumo <youmozhao@gmail.com>
2019-04-05 01:55:38 +0800

    update

--- PycharmProjects/selenium/test_case/SignIn.py
+++ PycharmProjects/selenium/test_case/SignIn.py
@@ -8,7 +8,8 @@ __author__ = 'Youmo'
 
 
 class SignIn(unittest.TestCase):
-    u"登录"
+    u"""登录"""
+
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
@@ -17,14 +18,14 @@ class SignIn(unittest.TestCase):
         time.sleep(1)
 
     def SignIn(self, username, psw):
-        u'''账号和密码参数化'''
+        u"""账号和密码参数化"""
         self.driver.find_element_by_id("account").send_keys(username)
         self.driver.find_element_by_id("password").send_keys(psw)
         self.driver.find_element_by_xpath("//div/div/button").click()
         time.sleep(3)
 
     def is_SignIn_sucess(self):
-        u'''判断是否获取到登录账户名称'''
+        u"""判断是否获取到登录账户名称"""
         try:
             text = self.driver.find_element_by_xpath("//li/div").text
             print text
@@ -33,24 +34,24 @@ class SignIn(unittest.TestCase):
             return False
 
     def test_01(self):
-        u'''正确账户，正确密码'''
+        u"""正确账户，正确密码"""
         self.SignIn("zhaoyoumo@outlook.com", "Abcd1234**")  # 调用登录方法
-        text = self.driver.find_element_by_xpath("//li/div").text   # 获取登录后的账号名称
+        text = self.driver.find_element_by_xpath("//li/div").text  # 获取登录后的账号名称
         print text
-        self.assertEqual(text, "zha******@outlook.com") # 断言实际结果与期望结果一致
+        self.assertEqual(text, "zha******@outlook.com")  # 断言实际结果与期望结果一致
         print u"登录成功！"
 
     def test_02(self):
-        u'''正确账户，错误密码'''
+        u"""正确账户，错误密码"""
         self.SignIn("zhaoyoumo@outlook.com", "**dddd")  # 调用登录方法
-        error_message = self.driver.find_element_by_xpath("//span/div/div/div/div/div").text    # 获取错误提示
+        error_message = self.driver.find_element_by_xpath("//span/div/div/div/div/div").text  # 获取错误提示
         self.assertTrue(error_message)  # 断言实际结果与期望结果一致
         print (u"弹出错误提示：%s" % error_message)
 
     def test_03(self):
-        u'''错误账户，错误密码'''
+        u"""错误账户，错误密码"""
         self.SignIn("123@qq.com", "**dddd")  # 调用登录方法
-        error_message = self.driver.find_element_by_xpath("//span/div/div/div/div/div").text    # 获取错误提示
+        error_message = self.driver.find_element_by_xpath("//span/div/div/div/div/div").text  # 获取错误提示
         self.assertTrue(error_message)  # 断言实际结果与期望结果一致
         print (u"弹出错误提示：%s" % error_message)
 
@@ -60,7 +61,3 @@ class SignIn(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
-
-
-
