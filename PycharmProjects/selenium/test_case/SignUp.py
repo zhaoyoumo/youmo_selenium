c168f82e23878f3fb7360aeac61f15603674dc3f
zhaoyoumo <495889504@qq.com>
2019-05-14 11:00:40 +0800

    update

--- PycharmProjects/selenium/test_case/SignUp.py
+++ PycharmProjects/selenium/test_case/SignUp.py
@@ -19,168 +19,187 @@ class SignUp(unittest.TestCase):
 
     def test01(self):
         u"""邮箱为空"""
-        self.driver.find_element_by_class("btn btn-outline-secondary btn-sm").cilck()
+        print ('----------Test01 Start----------')
         self.driver.find_element_by_id("password").send_keys("Abcd1234**")
-        self.driver.find_element_by_id("rePassword").send_keys("Abcd1234**")
-        self.driver.find_element_by_id("registerBtn']").click()
+        self.driver.find_element_by_name("confirmedPassword").send_keys("Abcd1234**")
+        self.driver.find_element_by_id("registerBtn").click()
         text = self.driver.find_element_by_css_selector(".due-message-text").text
         self.assertTrue(text)
         print (u'弹出错误提示：%s' % text)
+        print ('----------Test01 Passed----------')
 
     def test02(self):
         u"""密码为空"""
-        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
-#        self.driver.find_element_by_id("password").click()
-        self.driver.find_element_by_id("rePassword").send_keys("Abcd1234**")
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
+        print ('----------Test02 Start----------')
+        self.driver.find_element_by_name("email").send_keys("zhaoyoumo@outlook.com")
+        self.driver.find_element_by_name("confirmedPassword").send_keys("Abcd1234**")
+        self.driver.find_element_by_id("registerBtn").click()
         text = self.driver.find_element_by_css_selector(".due-message-text").text
         self.assertTrue(text)
         print (u'弹出错误提示：%s' % text)
+        print ('----------Test02 Passed----------')
 
     def test03(self):
         u"""密码为纯数字"""
-        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
-        self.driver.find_element_by_id("password").send_keys("1234567890")
-        self.driver.find_element_by_id("rePassword").send_keys("Abcd1234**")
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
+        print ('----------Test03 Start----------')
+        self.driver.find_element_by_name("email").send_keys("zhaoyoumo@outlook.com")
+        self.driver.find_element_by_id("password").send_keys("12345678")
+        self.driver.find_element_by_name("confirmedPassword").send_keys("12345678")
+        self.driver.find_element_by_id("registerBtn").click()
         text = self.driver.find_element_by_css_selector(".due-message-text").text
         self.assertTrue(text)
         print (u'弹出错误提示：%s' % text)
+        print ('----------Test03 Passed----------')
 
     def test04(self):
         u"""密码为纯字母"""
-        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
+        print ('----------Test04 Start----------')
+        self.driver.find_element_by_name("email").send_keys("zhaoyoumo@outlook.com")
         self.driver.find_element_by_id("password").send_keys("abcdefghijk")
-        self.driver.find_element_by_id("rePassword").send_keys("abcdefghijk")
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
+        self.driver.find_element_by_name("confirmedPassword").send_keys("abcdefghijk")
+        self.driver.find_element_by_id("registerBtn").click()
         text = self.driver.find_element_by_css_selector(".due-message-text").text
         self.assertTrue(text)
         print (u'弹出错误提示：%s' % text)
+        print ('----------Test04 Passed----------')
 
     def test05(self):
         u"""密码为纯符号"""
-        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
+        print ('----------Test05 Start----------')
+        self.driver.find_element_by_name("email").send_keys("zhaoyoumo@outlook.com")
         self.driver.find_element_by_id("password").send_keys("@@@@@@@@@@")
-        self.driver.find_element_by_id("rePassword").send_keys("@@@@@@@@@@")
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
+        self.driver.find_element_by_name("confirmedPassword").send_keys("@@@@@@@@@@")
+        self.driver.find_element_by_id("registerBtn").click()
         text = self.driver.find_element_by_css_selector(".due-message-text").text
         self.assertTrue(text)
         print (u'弹出错误提示：%s' % text)
+        print ('----------Test05 Passed----------')
 
     def test06(self):
         u"""密码为数字字母组合"""
-        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
+        print ('----------Test06 Start----------')
+        self.driver.find_element_by_name("email").send_keys("zhaoyoumo@outlook.com")
         self.driver.find_element_by_id("password").send_keys("1234abcdef")
-        self.driver.find_element_by_id("rePassword").send_keys("1234abcdef")
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
+        self.driver.find_element_by_name("confirmedPassword").send_keys("1234abcdef")
+        self.driver.find_element_by_id("registerBtn").click()
         text = self.driver.find_element_by_css_selector(".due-message-text").text
         self.assertTrue(text)
         print (u'弹出错误提示：%s' % text)
+        print ('----------Test06 Passed----------')
 
     def test07(self):
         u"""密码为数字符号组合"""
-        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
+        print ('----------Test07 Start----------')
+        self.driver.find_element_by_name("email").send_keys("zhaoyoumo@outlook.com")
         self.driver.find_element_by_id("password").send_keys("1234*****")
-        self.driver.find_element_by_id("rePassword").send_keys("1234*****")
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
+        self.driver.find_element_by_name("confirmedPassword").send_keys("1234*****")
+        self.driver.find_element_by_id("registerBtn").click()
         text = self.driver.find_element_by_css_selector(".due-message-text").text
         self.assertTrue(text)
         print (u'弹出错误提示：%s' % text)
+        print ('----------Test07 Passed----------')
 
     def test08(self):
         u"""密码为字母符号组合"""
-        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
+        print ('----------Test08 Start----------')
+        self.driver.find_element_by_name("email").send_keys("zhaoyoumo@outlook.com")
         self.driver.find_element_by_id("password").send_keys("abcd*****")
-        self.driver.find_element_by_id("rePassword").send_keys("abcd*****")
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
+        self.driver.find_element_by_name("confirmedPassword").send_keys("abcd*****")
+        self.driver.find_element_by_id("registerBtn").click()
         text = self.driver.find_element_by_css_selector(".due-message-text").text
         self.assertTrue(text)
         print (u'弹出错误提示：%s' % text)
+        print ('----------Test08 Passed----------')
 
     def test09(self):
         u"""密码为7位"""
-        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
+        print ('----------Test09 Start----------')
+        self.driver.find_element_by_name("email").send_keys("zhaoyoumo@outlook.com")
         self.driver.find_element_by_id("password").send_keys("Acbd12*")
-        self.driver.find_element_by_id("rePassword").send_keys("Acbd12*")
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
+        self.driver.find_element_by_name("confirmedPassword").send_keys("Acbd12*")
+        self.driver.find_element_by_id("registerBtn").click()
         text = self.driver.find_element_by_css_selector(".due-message-text").text
         self.assertTrue(text)
         print (u'弹出错误提示：%s' % text)
+        print ('----------Test09 Passed----------')
 
 
     def test10(self):
         u"""密码为21位"""
-        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
+        print ('----------Test10 Start----------')
+        self.driver.find_element_by_name("email").send_keys("zhaoyoumo@outlook.com")
         self.driver.find_element_by_id("password").send_keys("Aaaaaaaaaa99999*****0")
-        self.driver.find_element_by_id("rePassword").send_keys("Aaaaaaaaaa99999*****0")
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
+        self.driver.find_element_by_name("confirmedPassword").send_keys("Aaaaaaaaaa99999*****0")
+        self.driver.find_element_by_id("registerBtn").click()
         text = self.driver.find_element_by_css_selector(".due-message-text").text
         self.assertTrue(text)
         print (u'弹出错误提示：%s' % text)
+        print ('----------Test10 Passed----------')
 
     def test11(self):
         u"""确认密码为空"""
-        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
+        print ('----------Test11 Start----------')
+        self.driver.find_element_by_name("email").send_keys("zhaoyoumo@outlook.com")
         self.driver.find_element_by_id("password").send_keys("Abcd1234**")
-        self.driver.find_element_by_id("rePassword").click()
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
+        self.driver.find_element_by_id("registerBtn").click()
         text = self.driver.find_element_by_css_selector(".due-message-text").text
         self.assertTrue(text)
         print (u'弹出错误提示：%s' % text)
-
-    def test12(self):
-        u"""确认密码输入错误"""
-        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
-        self.driver.find_element_by_id("password").send_keys("Abcd1234**")
-        self.driver.find_element_by_id("rePassword").send_keys("1234Abcd&&")
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
-        text = self.driver.find_element_by_css_selector(".due-message-text").text
-        self.assertTrue(text)
-        print (u'弹出错误提示：%s' % text)
-
-    def test13(self):
-        u"""输入错误邀请码"""
-        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
-        self.driver.find_element_by_id("password").send_keys("Abcd1234**")
-        self.driver.find_element_by_id("rePassword").send_keys("Abcd1234**")
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/form/div[5]/input").send_keys(
-            "AAAAAA")
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
-        text = self.driver.find_element_by_css_selector(".due-message-text").text
-        self.assertTrue(text)
-        print (u'弹出错误提示：%s' % text)
-
-    def test14(self):
-        u"""未勾选注册协议"""
-        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
-        self.driver.find_element_by_id("password").send_keys("Abcd1234**")
-        self.driver.find_element_by_id("rePassword").send_keys("Abcd1234**")
-        # self.driver.find_element_by_xpath(
-        #     "//div[@id='root']/div/div[2]/div/div/div/div/form/div[6]/div/label").click()  # 勾选《注册协议》
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
-        text = self.driver.find_element_by_css_selector(".due-message-text").text
-        self.assertTrue(text)
-        print (u'弹出错误提示：%s' % text)
-
-    def test15(self):
-        u"""邮箱已注册"""
-        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("zhaoyomo@outlook.com")
-        self.driver.find_element_by_id("password").send_keys("Abcd1234**")
-        self.driver.find_element_by_id("rePassword").send_keys("Abcd1234**")
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
-        text = self.driver.find_element_by_css_selector(".due-message-text").text
-        self.assertTrue(text)
-        print (u'弹出错误提示：%s' % text)
-
-    def test16(self):
-        u"""邮箱错误"""
-        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("zhaoyomo@.com")
-        self.driver.find_element_by_id("password").send_keys("Abcd1234**")
-        self.driver.find_element_by_id("rePassword").send_keys("Abcd1234**")
-        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
-        text = self.driver.find_element_by_css_selector(".due-message-text").text
-        self.assertTrue(text)
-        print (u'弹出错误提示：%s' % text)
+        print ('----------Test11 Passed----------')
+#
+#    def test12(self):
+#        u"""确认密码输入错误"""
+#        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
+#        self.driver.find_element_by_id("password").send_keys("Abcd1234**")
+#        self.driver.find_element_by_id("rePassword").send_keys("1234Abcd&&")
+#        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
+#        text = self.driver.find_element_by_css_selector(".due-message-text").text
+#        self.assertTrue(text)
+#        print (u'弹出错误提示：%s' % text)
+#
+#    def test13(self):
+#        u"""输入错误邀请码"""
+#        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
+#        self.driver.find_element_by_id("password").send_keys("Abcd1234**")
+#        self.driver.find_element_by_id("rePassword").send_keys("Abcd1234**")
+#        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/form/div[5]/input").send_keys(
+#            "AAAAAA")
+#        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
+#        text = self.driver.find_element_by_css_selector(".due-message-text").text
+#        self.assertTrue(text)
+#        print (u'弹出错误提示：%s' % text)
+#
+#    def test14(self):
+#        u"""未勾选注册协议"""
+#        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("abc1234@qq.com")
+#        self.driver.find_element_by_id("password").send_keys("Abcd1234**")
+#        self.driver.find_element_by_id("rePassword").send_keys("Abcd1234**")
+#        # self.driver.find_element_by_xpath(
+#        #     "//div[@id='root']/div/div[2]/div/div/div/div/form/div[6]/div/label").click()  # 勾选《注册协议》
+#        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
+#        text = self.driver.find_element_by_css_selector(".due-message-text").text
+#        self.assertTrue(text)
+#        print (u'弹出错误提示：%s' % text)
+#
+#    def test15(self):
+#        u"""邮箱已注册"""
+#        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("zhaoyomo@outlook.com")
+#        self.driver.find_element_by_id("password").send_keys("Abcd1234**")
+#        self.driver.find_element_by_id("rePassword").send_keys("Abcd1234**")
+#        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
+#        text = self.driver.find_element_by_css_selector(".due-message-text").text
+#        self.assertTrue(text)
+#        print (u'弹出错误提示：%s' % text)
+#
+#    def test16(self):
+#        u"""邮箱错误"""
+#        self.driver.find_element_by_xpath("//input[@type='email']").send_keys("zhaoyomo@.com")
+#        self.driver.find_element_by_id("password").send_keys("Abcd1234**")
+#        self.driver.find_element_by_id("rePassword").send_keys("Abcd1234**")
+#        self.driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/button/span").click()
+#        text = self.driver.find_element_by_css_selector(".due-message-text").text
+#        self.assertTrue(text)
+#        print (u'弹出错误提示：%s' % text)
 
     def tearDown(self):
         self.driver.quit()
